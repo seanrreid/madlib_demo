0cb953cf4eb6558d1a3c6f67cb607b002218209b
Sean R Reid <sean@torchcodelab.com>
2021-01-11 14:54:54 -0500

    Version 2, following the update exercise rules. Uses forEach() loops and an array.

--- js/index.js
+++ js/index.js
@@ -6,16 +6,20 @@ const generateMadlib = document.querySelector('#generateMadlib');
 
 generateMadlib.addEventListener('submit', function (e) {
     e.preventDefault();
+    // Select all inputs and our placeholders using the method `querySelectorAll()`
     const inputSelectors = document.querySelectorAll('input');
-    const myName = document.querySelector('#myName');
-    const mySubject = document.querySelector('#mySubject');
+    const placeholders = document.querySelectorAll('.madlib_placeholder');
 
+    // Use a `forEach` loop to create an array of values for our replacement words
+    const inputArray = [];
     inputSelectors.forEach(function (inputItem) {
-        if (inputItem.name === 'myNameInput') {
-            myName.innerHTML = inputItem.value;
-        }
-        if (inputItem.name === 'mySubjectInput') {
-            mySubject.innerHTML = inputItem.value;
-        }
+        inputArray.push(inputItem);
+    });
+
+    // Use a `forEach` loop to update the `innerHTML` of each placeholder
+    // The `forEach()` method takes `index` as an agrgument,
+    // which will return the index of a given item in the loop.
+    placeholders.forEach(function (placeholder, index) {
+        placeholder.innerHTML = inputArray[index].value;
     });
 });
